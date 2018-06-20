8ce1ea4074c31886e0ae79a7af97a81dcc3cab21
mlazos <matias.lazos@gmail.com>
2018-06-20 01:28:34 -0400

    Redefiniendo el contrato, y llamandolo desde la vista

--- QuestionContract.js
+++ QuestionContract.js
@@ -1,30 +1,20 @@
 "use strict";
 
-//var contract_address = "n1zgSuH6jyeyj2CCKDSR8jbwyBRVkhfBi5f";
+//var contract_address = "n1zsH1HnWbQPw7rLRxc9CRw5gjT6qfzyt7P";
 
-var QuestionItem = class{
-    constructor(question_id,author,title,answers){
-        this.question_id = question_id;
-        this.author = author;
-        this.title = title;
-        this.answers = answers;     
-        this.date = Date.now();
-    }
+var AnswerItem = function(item){
+    this.item = item;
+    this.count_votes = 0;
 };
 
-function AnswerItemsObj(data)
-{
-    if(data){
-        var answers = [];
-        var item = null;
-        for( var i=0; i<data.length; i++){
-            item = {answer: data[i], count_votes: 0}
-            answers.push(item);
-        }
-        return answers;
-    }
-}
-
+var QuestionItem = function(question_id,author,title,answers){
+    this.question_id = question_id;
+    this.author = author;
+    this.title = title;
+    this.answers = answers;     
+    this.date = Date.now();
+    this.total_votes = 0;
+};
 
 var QuestionContract = function () {
     LocalContractStorage.defineMapProperty(this,"hash_question");
@@ -33,24 +23,21 @@ var QuestionContract = function () {
 };
 
 QuestionContract.prototype = {
-	//Init is called once, when the contract is deployed
-    init: function() {
-        this.question_id = 1; //The first id should be 1 (not 0)
 
-        var from_address = Blockchain.transaction.from;
-        var answer = [{"answer":"Yes","count_votes":1},{"answer":"No","count_votes":0}];
+    init: function() { //Init is called once, when the contract is deployed
+
+        this.question_id = 1;
+
+        //create first question
         var initialQuestion = new QuestionItem(
             this.question_id,
-            from_address,
+            Blockchain.transaction.from,
             "Would you like to try 'Votes', a Dapps using NAS red",
-            answer
+            new Array(new AnswerItem("Yes"),new AnswerItem("No"))
         );
+
         //put question
-        this.hash_question.put(this.question_id, initialQuestion);
-        //put myVote
-        var my_votes = []
-        my_votes.push(this.question_id);
-        this.hash_my_votes.put(Blockchain.transaction.from,my_votes);
+        this.hash_question.put(this.question_id, initialQuestion);        
         this.question_id++;
     },
 
@@ -66,12 +53,15 @@ QuestionContract.prototype = {
         else if(answers.length < 2 || answers.length > 5){
             throw new Error("You must enter between 2 and 5 answers");
         }
-
         if(Blockchain.transaction.value < 0){
     		throw new Error("You don't need pay to post a question");
         }
 
-        var answerItems = AnswerItemsObj(answers);
+        //answers = ["item1","item2","item3"]
+        var answerItems = [];
+        for(var i=0; i< answers.length; i++){
+            answerItems.push(new AnswerItem(answers[i]));
+        }
 
         var question = new QuestionItem(
             this.question_id,
@@ -108,6 +98,7 @@ QuestionContract.prototype = {
 
         var from_address = null;
         var question = null;
+        var my_votes = [];
 
         //Chequear que la alternativa seleccionada no este vacia
         option = option.trim();
@@ -116,13 +107,13 @@ QuestionContract.prototype = {
         }
 
         //Chequear existencia de votos anteriores
-        from_address = Blockchain.transaction.from;
-        //Obtener mis votos
-        var my_votes = [];
+        from_address = Blockchain.transaction.from;  
         my_votes = this.hash_my_votes.get(from_address);
-        if(my_votes){ //Si tengo votos, verificar que yo no haya respondido esta pregunta
+        if(my_votes){ 
+            //Si tengo votos, verificar que yo no haya respondido esta pregunta
             for(var i=0; i< my_votes.length; i++){
-                if(my_votes[i] == question_id){ // Ya vote en esta pregunta
+                if(my_votes[i] == question_id){ 
+                    // Ya vote en esta pregunta
                     throw new Error("You already voted on this question");
                 }
             }
@@ -135,7 +126,7 @@ QuestionContract.prototype = {
         //Chequear que la alternativa seleccionada exista como respuesta
         var found_option = false;
         for(var i = 0; i< question.answers.length; i++){
-            if(question.answers[i].answer == option){
+            if(question.answers[i].item == option){
                 found_option = true;
                 question.answers[i].count_votes ++;
                 question.total_votes ++;
